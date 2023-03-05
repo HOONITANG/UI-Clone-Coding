04feebb2659dc3730cd595fbc875bf639dac353a
taehoon <rlaxogns952@gmail.com>
2023-03-05 16:56:49 +0900

    xcode project 생성

---
+++ TadaClone/TadaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TadaClone
+//
+//  Created by Taehoon Kim on 2023/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
